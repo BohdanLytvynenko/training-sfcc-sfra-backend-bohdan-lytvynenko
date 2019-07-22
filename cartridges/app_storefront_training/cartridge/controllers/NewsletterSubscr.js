6b0c7f1e3077870a74e68ea65b06fbe5a8c0259a
bohdan_lytvynenko <bohdan.lytvynenko@osf-commerce.com>
2019-07-22 18:42:15 +0300

    CATS-555 5.5 Newsletter
    - created new xml form
    - created templated for the form
    - got form in controller
    - created newsletter custom object and store there the data from form, using transaction
    - created coupon code and campaign for it
    - assigned new coupon code every time when user subscribe in newsletter
    - stored this code in custom object

--- cartridges/app_storefront_training/cartridge/controllers/NewsletterSubscr.js
+++ cartridges/app_storefront_training/cartridge/controllers/NewsletterSubscr.js
@@ -6,7 +6,6 @@ var URLUtils = require('dw/web/URLUtils');
 var CustomObjectMgr = require('dw/object/CustomObjectMgr');
 
 var actionUrl = URLUtils.url('NewsletterSubscr-HandleForm');
-// var CustomObjectMgr = require('dw/object/CustomObjectMgr');
 
 server.get('Start', function (req, res, next) {
 	var newsletterForm = server.forms.getForm('newsletter'); // name the xml!
@@ -23,43 +22,57 @@ server.post('HandleForm', function (req, res, next) {  // form gets validated he
 	var newsletterForm = server.forms.getForm('newsletter'); // name the xml!
 	var Transaction = require('dw/system/Transaction');
 	var CouponMgr = require('dw/campaign/CouponMgr');
+	var Mail = require('dw/net/Mail');
+	var HashMap = require('dw/util/HashMap');
+	var Resource = require('dw/web/Resource');
+	var Template = require('dw/util/Template');
+	var Site = require('dw/system/Site');
+	var couponCode;
+
 	var myCoupon = CouponMgr.getCoupon('20_off');
-	// var couponCode = myCoupon.getNextCouponCode();
 	
 	Transaction.wrap(function() {
-		var customObjectInstance = CustomObjectMgr.createCustomObject("NewsletterSubscr", newsletterForm.email.htmlValue.toString());	
-		customObjectInstance.custom.firstName = newsletterForm.firstname.htmlValue.toString();		
-		customObjectInstance.custom.lastName = newsletterForm.lastname.htmlValue.toString();		
-		customObjectInstance.custom.couponCode = myCoupon.getNextCouponCode();
+		try {
+			// creating custom object
+			var customObjectInstance = CustomObjectMgr.createCustomObject("NewsletterSubscr", newsletterForm.email.htmlValue.toString());	
+			customObjectInstance.custom.firstName = newsletterForm.firstname.htmlValue.toString();		
+			customObjectInstance.custom.lastName = newsletterForm.lastname.htmlValue.toString();
+
+			couponCode = myCoupon.getNextCouponCode();
+			customObjectInstance.custom.couponCode = couponCode;
+
+			// sending message with promocode
+			var userObjectForEmail = {
+				firstNameField: newsletterForm.firstname.htmlValue.toString(),
+				lastNameField: newsletterForm.lastname.htmlValue.toString(),
+				couponCode: couponCode
+			};
+
+			var newsletterEmail = new Mail();
+			var context = new HashMap();
+
+			Object.keys(userObjectForEmail).forEach(function (key) {
+				context.put(key, userObjectForEmail[key]);
+			});
+
+			newsletterEmail.addTo(newsletterForm.email.htmlValue.toString());
+			newsletterEmail.setSubject(Resource.msg('subject.newsletter.email', 'mail', null));
+			newsletterEmail.setFrom(
+					Site.current.getCustomPreferenceValue('customerServiceEmail')
+					|| 'no-reply@salesforce.com');
+
+			var template = new Template('newsletterMail');
+			var content = template.render(context).text;
+			newsletterEmail.setContent(content, 'text/html', 'UTF-8');
+			newsletterEmail.send();
+
+			res.render('home/homePage');
+		}
+		catch(e) {
+			res.render('newsletterError');
+		}
 	});
 
-	// var userObjectForEmail = {
-    //     firstNameField: firstNameField,
-    //     lastNameField: lastNameField,
-    //     couponCode: couponCode
-    // };
-
-    // var newsletterEmail = new Mail();
-    // var context = new HashMap();
-
-    // Object.keys(userObjectForEmail).forEach(function (key) {
-    //     context.put(key, userObjectForEmail[key]);
-    // });
-
-    // newsletterEmail.addTo(emailField);
-    // newsletterEmail.setSubject(Resource.msg('subject.productAdded.email', 'mail', null));
-	// newsletterEmail.setFrom(
-    //         Site.current.getCustomPreferenceValue('customerServiceEmail')
-    //         || 'no-reply@salesforce.com');
-
-    // var template = new Template('newsletterMail');
-    // var content = template.render(context).text;
-    // newsletterEmail.setContent(content, 'text/html', 'UTF-8');
-    // newsletterEmail.send();
-
-	// check for any other form actions
-	// otherwise...
-	res.render('/home/homePage');
 	next();
 });
 
